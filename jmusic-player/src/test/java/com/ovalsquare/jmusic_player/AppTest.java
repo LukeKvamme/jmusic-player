29ad7f3a24a7c4c5ef53caae199e8dfa27e87909
lukek <lukek@10.0.0.151>
2024-03-08 08:35:06 -0500

    initial commit

---
+++ jmusic-player/src/test/java/com/ovalsquare/jmusic_player/AppTest.java
@@ -0,0 +1,20 @@
+package com.ovalsquare.jmusic_player;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
