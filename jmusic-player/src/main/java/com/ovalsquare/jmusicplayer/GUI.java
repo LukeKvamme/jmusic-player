4ce5e1e5b0279d3e9cf8c894683346a27a7d7526
lukek <lukek@10.0.0.151>
2024-03-09 10:29:13 -0500

    GUI plays audio of .mp3 files

--- jmusic-player/src/main/java/com/ovalsquare/jmusicplayer/GUI.java
+++ jmusic-player/src/main/java/com/ovalsquare/jmusicplayer/GUI.java
@@ -1,34 +1,52 @@
 package com.ovalsquare.jmusicplayer;
 
+import java.net.URISyntaxException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
 	public static void main(String[] args) {
         launch(args);
+        
     }
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    	Media media = null;
+        try {
+          media = new Media(getClass().getResource("/vampire heart (slowed  reverb).mp3").toURI().toString());
+          MediaPlayer mediaPlayer = new MediaPlayer(media);
+          mediaPlayer.play();
+        } catch (URISyntaxException e) {
+          e.printStackTrace();
+        } 
+//        primaryStage.setTitle("Hello World!");
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+// 
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        primaryStage.setScene(new Scene(root, 300, 250));
+//        primaryStage.show();
+
     }
+    
+    
+    
+    
 }
--- jmusic-player/src/main/java/module-info.java
+++ jmusic-player/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module jmusicplayer {
 	requires javafx.controls;
 	requires mp3agic;
 	requires transitive javafx.graphics;
+	requires java.desktop;
+	requires javafx.media;
 	//requires junit;
 	
 	exports com.ovalsquare.jmusicplayer to javafx.graphics;
