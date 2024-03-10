dc8444f991911afcf20b378606e98f52305c0b62
lukek <lukek@10.0.0.151>
2024-03-10 09:26:05 -0400

    Audio, ListView, Pause/Resume
    Audio Works with two underlying ListViews: One displays the Song._title,
    one saves the Song object. Clicking the Song title in the ListView
    selects the Song object and plays it that way.
    
    Changed the Play and Pause buttons to ToggleButtons so when a song is
    paused, hitting play resumes the song. Previously, hitting Play would
    restart the song.

--- jmusic-player/src/main/java/com/ovalsquare/jmusicplayer/App.java
+++ jmusic-player/src/main/java/com/ovalsquare/jmusicplayer/App.java
@@ -15,9 +15,9 @@ public class App
 
     public static void main( String[] args ) throws UnsupportedTagException, InvalidDataException, IOException
     {
-//    	SongHandler sh = new SongHandler("A:\\");
+//    	SongHandler sh = new SongHandler();
 //    	ArrayList<Song> songs = sh.createSongList();
-//    	
+    	
 //    	for (Song s : songs) {
 //    		System.out.println(s.get_songPath());
 //    	}
--- jmusic-player/src/main/java/com/ovalsquare/jmusicplayer/GUI.java
+++ jmusic-player/src/main/java/com/ovalsquare/jmusicplayer/GUI.java
@@ -9,7 +9,10 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
@@ -20,62 +23,106 @@ import javafx.stage.Stage;
 
 public class GUI extends Application {
 
-	    private ListView<String> _fileList;
-	    private MediaPlayer _mediaPlayer;
+	private static SongHandler _songHandler;
+	private ListView<String> _musicListView;
+	private ListView<Song> _musicFiles;
+    private MediaPlayer mediaPlayer;
+    private ToggleButton playButton;
+    private ToggleButton pauseButton;
+    private ToggleGroup _group;
+    private Button stopButton;
+    private Button queueNextButton;
+    private Button queueLastButton;
 
-	    @Override
-	    public void start(Stage primaryStage) {
-	        primaryStage.setTitle("Jmusic Player");
+    @Override
+    public void start(Stage primaryStage) {
+    	primaryStage.setTitle("JMusic Player");
+    	_songHandler = new SongHandler();
+    	_musicListView = new ListView<>();
+    	_musicFiles = new ListView<>();
+    	updateFileList();
 
-	        _fileList = new ListView<>();
-	        updateFileList(new File(System.getProperty("user.home").concat("\\Music")));
+        playButton = new ToggleButton("Play");
+        playButton.setOnAction(event -> playSelectedFile());
 
-	        _fileList.setOnMouseClicked(event -> {
-	            String selected_item = _fileList.getSelectionModel().getSelectedItem();
-	            if (selected_item != null) {
-	                String file_path = System.getProperty("user.home").concat("\\Music") + File.separator + selected_item;
-	                File file = new File(file_path);
-	                if (file.isFile() && file.getName().toLowerCase().endsWith(".mp3")) {
-	                    playMP3(file);
-	                }
-	            }
-	        });
+        pauseButton = new ToggleButton("Pause");
+        pauseButton.setOnAction(event -> pausePlayback());
+        
+        playButton.setToggleGroup(_group);
+        pauseButton.setToggleGroup(_group);
 
-	        VBox file_list_view = new VBox(_fileList);
-	        
-	        BorderPane root = new BorderPane();
-	        root.setRight(file_list_view);
+        stopButton = new Button("Stop");
+        stopButton.setOnAction(event -> stopPlayback());
 
-	        Scene scene = new Scene(root, 500, 400);
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-	    }
+        queueNextButton = new Button("Queue Next");
+        // Add functionality for queuing next
 
-	    private void updateFileList(File directory) {
-	        _fileList.getItems().clear();
-	        File[] files = directory.listFiles();
-	        
-	        if (files != null) {
-	            for (File file : files) {
-	            	if (file.getName().toLowerCase().endsWith(".mp3")) {
-	            		_fileList.getItems().add(file.getName());
-	            	}
-	            }
-	        }
-	    }
+        queueLastButton = new Button("Queue Last");
+        // Add functionality for queuing last
 
-	    private void playMP3(File file) {
-	        if (_mediaPlayer != null) {
-	            _mediaPlayer.stop();
-	        }
-	        Media media = new Media(file.toURI().toString());
-	        _mediaPlayer = new MediaPlayer(media);
-	        _mediaPlayer.play();
-	    }
+        HBox controls = new HBox(playButton, pauseButton, stopButton, queueNextButton, queueLastButton);
 
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
-	}
+        BorderPane root = new BorderPane();
+        root.setLeft(_musicListView);
+        root.setBottom(controls);
+
+        Scene scene = new Scene(root, 600, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void updateFileList() {
+        _musicListView.getItems().clear();
+        _musicFiles.getItems().clear();
+        
+        for (Song song : _songHandler._songList) {
+        	_musicListView.getItems().add(song.get_title());
+        	_musicFiles.getItems().add(song);
+        }
+    	
+    }
+
+    private void playSelectedFile() {
+    	Song selected_song = _musicFiles.getItems().get(_musicListView.getSelectionModel().getSelectedIndex());
+//    	System.out.println(selected_song);
+//    	System.out.println(_musicFiles.getItems());
+    	if (pauseButton.isSelected()) {
+    		mediaPlayer.play();
+    		playButton.setSelected(true);
+    		pauseButton.setSelected(false);
+    	}
+    	else if (_songHandler._songList.contains(selected_song)) {
+    		File song_mp3_file = new File(selected_song.get_songPath());
+    		stopPlayback();
+    		
+    		Media media = new Media(song_mp3_file.toURI().toString());
+    		mediaPlayer = new MediaPlayer(media);
+    		mediaPlayer.play();
+    		playButton.setSelected(true);
+    		pauseButton.setSelected(false);
+    	}
+    	else {
+    		System.err.println("Something catastrophic occurred.");
+    	}
+    }
+
+    private void pausePlayback() {
+        if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+            mediaPlayer.pause();
+            playButton.setSelected(false);
+            pauseButton.setSelected(true);
+        }
+    }
+
+    private void stopPlayback() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
 
 
