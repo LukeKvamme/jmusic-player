9c0f5d9d931297a469d7fb7b469f4da237e8a2e6
lukek <lukek@10.0.0.151>
2024-03-10 07:14:07 -0400

    Music Folder Browser
    Lists the content inside the C:\Users\xxx\Music folder, the Windows
    default location for music.

--- jmusic-player/src/main/java/com/ovalsquare/jmusicplayer/GUI.java
+++ jmusic-player/src/main/java/com/ovalsquare/jmusicplayer/GUI.java
@@ -1,5 +1,6 @@
 package com.ovalsquare.jmusicplayer;
 
+import java.io.File;
 import java.net.URISyntaxException;
 
 import javafx.application.Application;
@@ -7,46 +8,68 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
-	public static void main(String[] args) {
-        launch(args);
-        
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-    	Media media = null;
-        try {
-          media = new Media(getClass().getResource("/vampire heart (slowed  reverb).mp3").toURI().toString());
-          MediaPlayer mediaPlayer = new MediaPlayer(media);
-          mediaPlayer.play();
-        } catch (URISyntaxException e) {
-          e.printStackTrace();
-        } 
-//        primaryStage.setTitle("Hello World!");
-//        Button btn = new Button();
-//        btn.setText("Say 'Hello World'");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-// 
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        });
-//        
-//        StackPane root = new StackPane();
-//        root.getChildren().add(btn);
-//        primaryStage.setScene(new Scene(root, 300, 250));
-//        primaryStage.show();
 
-    }
-    
-    
-    
-    
-}
+	    private ListView<String> fileList;
+	    private MediaPlayer mediaPlayer; 
+
+	    @Override
+	    public void start(Stage primaryStage) {
+	        primaryStage.setTitle("File Browser with MediaPlayer");
+
+	        fileList = new ListView<>();
+	        updateFileList(new File(System.getProperty("user.home").concat("\\Music")));
+
+	        fileList.setOnMouseClicked(event -> {
+	            String selectedItem = fileList.getSelectionModel().getSelectedItem();
+	            if (selectedItem != null) {
+	                String filePath = System.getProperty("user.home") + File.separator + selectedItem;
+	                File file = new File(filePath);
+	                if (file.isFile() && file.getName().toLowerCase().endsWith(".mp3")) {
+	                    playMP3(file);
+	                }
+	            }
+	        });
+
+	        VBox root = new VBox(fileList);
+
+	        Scene scene = new Scene(root, 400, 300);
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+	    }
+
+	    private void updateFileList(File directory) {
+	        fileList.getItems().clear();
+	        File[] files = directory.listFiles();
+	        if (files != null) {
+	            for (File file : files) {
+	                fileList.getItems().add(file.getName());
+	            }
+	        }
+	    }
+
+	    private void playMP3(File file) {
+	        if (mediaPlayer != null) {
+	            mediaPlayer.stop();
+	        }
+	        Media media = new Media(file.toURI().toString());
+	        mediaPlayer = new MediaPlayer(media);
+	        mediaPlayer.play();
+	    }
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	}
+
+
